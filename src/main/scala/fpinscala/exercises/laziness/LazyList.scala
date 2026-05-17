b9d4edcaed2e5275a76e4b6726770d3f876d518f
NeverOoO <qilongzhen@163.com>
2026-05-18 06:18:36 +0800

    my own answer on lazylist and state
    my own answer on lazylist and state

--- src/main/scala/fpinscala/exercises/laziness/LazyList.scala
+++ src/main/scala/fpinscala/exercises/laziness/LazyList.scala
@@ -1,10 +1,15 @@
 package fpinscala.exercises.laziness
 
+import fpinscala.exercises.laziness.LazyList.*
+
 enum LazyList[+A]:
   case Empty
   case Cons(h: () => A, t: () => LazyList[A])
 
-  def toList: List[A] = ???
+  def toList: List[A] = this match {
+    case Empty => List.empty
+    case Cons(h, t) => h() :: t().toList
+  }
 
   def foldRight[B](z: => B)(f: (A, => B) => B): B = // The arrow `=>` in front of the argument type `B` means that the function `f` takes its second argument by name and may choose not to evaluate it.
     this match
@@ -19,20 +24,126 @@ enum LazyList[+A]:
     case Empty => None
     case Cons(h, t) => if (f(h())) Some(h()) else t().find(f)
 
-  def take(n: Int): LazyList[A] = ???
+  def take(n: Int): LazyList[A] = this match {
+    case Cons(h, t) if n > 1 => cons(h(), t().take(n - 1))
+    case Cons(h, _) if n == 1 => cons(h(), Empty)
+    case _ => Empty
+  }
 
-  def drop(n: Int): LazyList[A] = ???
+  def drop(n: Int): LazyList[A] = this match {
+    case Cons(_, t) if n > 0 => t().drop(n - 1)
+    case _ => this
+  }
 
-  def takeWhile(p: A => Boolean): LazyList[A] = ???
+  def takeWhile(p: A => Boolean): LazyList[A] = this match {
+    case Cons(h, t) if p(h()) => cons(h(), t().takeWhile(p))
+    case Cons(h, t) => t().takeWhile(p)
+    case _ => Empty
+  }
 
-  def forAll(p: A => Boolean): Boolean = ???
+  def forAll(p: A => Boolean): Boolean = this match {
+    case Cons(h, t) if p(h()) => t().forAll(p)
+    case Cons(_, _) => false
+    case Empty => true
+  }
 
-  def headOption: Option[A] = ???
+  def headOption: Option[A] = this match {
+    case Cons(h, _) => Some(h())
+    case _ => None
+  }
 
   // 5.7 map, filter, append, flatmap using foldRight. Part of the exercise is
   // writing your own function signatures.
 
-  def startsWith[B](s: LazyList[B]): Boolean = ???
+  def takeWhileByFold(p: A => Boolean): LazyList[A] =
+    foldRight(empty)((a, acc) => if p(a) then cons(a, acc) else Empty)
+
+  def headOptionByFold: Option[A] =
+    foldRight(None: Option[A])((a, acc) => Some(a))
+
+  def map[B](f: A => B): LazyList[B] =
+    foldRight(empty[B])((a, acc) => cons(f(a), acc))
+
+  def filter(f: A => Boolean): LazyList[A] =
+    foldRight(empty[A])((a, acc) => if f(a) then cons(a, acc) else acc)
+
+  def append[A2 >: A](that: => LazyList[A2]): LazyList[A2] =
+    foldRight(that)((a, acc) => cons(a, acc))
+
+  def flatMap[B](f: A => LazyList[B]): LazyList[B] =
+    foldRight(empty[B])((a, acc) => f(a).append(acc))
+
+  def mapViaUnfold[B](f: A => B): LazyList[B] =
+    unfold(this):
+      case Cons(h, t) => Some((f(h()), t()))
+      case _ => None
+
+  def takeViaUnfold(n: Int): LazyList[A] =
+    unfold((n, this)):
+      case (idx, Cons(h, t)) if (idx > 0) => Some((h(), (idx - 1, t())))
+      case _ => None
+
+  def takeWhileViaUnfold(f: A => Boolean): LazyList[A] =
+    unfold(this):
+      case Cons(h, t) if f(h()) => Some((h(), t()))
+      case _ => None
+
+  def zipAll[B](that: LazyList[B]): LazyList[(Option[A], Option[B])] =
+    unfold((this, that)):
+      case (Cons(h1, t1), Cons(h2, t2)) => Some((Some(h1()), Some(h2())), (t1(), t2()))
+      case (Empty, Cons(h2, t2)) => Some((None, Some(h2())), (Empty, t2()))
+      case (Cons(h1, t1), Empty) => Some((Some(h1()), None), (t1(), Empty))
+      case (Empty, Empty) => None
+
+  def zipWith[B, C](that: LazyList[B])(f: (A, B) => C): LazyList[C] =
+    unfold((this, that)):
+      case (Cons(h1, t1), Cons(h2, t2)) => Some((f(h1(), h2())), (t1(), t2()))
+      case (_, _) => None
+
+  // special case of `zipWith`
+  def zip[B](that: LazyList[B]): LazyList[(A, B)] =
+    zipWith(that)((_, _))
+
+  def zipWithAll[B, C](that: LazyList[B])(f: (Option[A], Option[B]) => C): LazyList[C] =
+    zipAll(that).map((a, b) => f(a, b))
+
+  def zipAllViaZipWithAll[B](s2: LazyList[B]): LazyList[(Option[A], Option[B])] =
+    zipWithAll(s2)((_, _))
+
+  /*
+  `s.startsWith(s2)` when corresponding elements of `s` and `s2` are all equal, until the point that `s2` is exhausted. If `s` is exhausted first, or we find an element that doesn't match, we terminate early. Using non-strictness, we can compose these three separate logical steps--the zipping, the termination when the second lazy list is exhausted, and the termination if a nonmatching element is found or the first lazy list is exhausted.
+  */
+  def startsWith[A](prefix: LazyList[A]): Boolean = (this, prefix) match {
+    case (Empty, Cons(h, _)) => false
+    case(_, Empty) => true
+    case(Cons(h1, t1), Cons(h2, t2)) if h1() == h2() => t1().startsWith(t2())
+    case (_, _) => false
+  }
+
+  /*
+  The last element of `tails` is always the empty `LazyList`, so we handle this as a special case, by appending it to the output.
+  */
+  def tails: LazyList[LazyList[A]] =
+    unfold(this):
+      case Empty => None
+      case Cons(h, t) => Some((Cons(h, t), t()))
+    .append(LazyList(empty))
+
+  def hasSubsequence[A](s: LazyList[A]): Boolean =
+    tails.exists(_.startsWith(s))
+
+  /*
+  The function can't be implemented using `unfold`, since `unfold` generates elements of the `LazyList` from left to right. It can be implemented using `foldRight` though.
+
+  The implementation is just a `foldRight` that keeps the accumulated value and the lazy list of intermediate results, which we `cons` onto during each iteration. When writing folds, it's common to have more state in the fold than is needed to compute the result. Here, we simply extract the accumulated list once finished.
+  */
+  def scanRight[B](init: B)(f: (A, => B) => B): LazyList[B] =
+    foldRight(init -> LazyList(init)): (a, b0) =>
+      // b0 is passed by-name and used in by-name args in f and cons. So use lazy val to ensure only one evaluation...
+      lazy val b1 = b0
+      val b2 = f(a, b1(0))
+      (b2, cons(b2, b1(1)))
+    ._2
 
 
 object LazyList:
@@ -49,18 +160,28 @@ object LazyList:
 
   val ones: LazyList[Int] = LazyList.cons(1, ones)
 
-  def continually[A](a: A): LazyList[A] = ???
+  def continually[A](a: A): LazyList[A] = LazyList.cons(a, continually(a))
 
-  def from(n: Int): LazyList[Int] = ???
+  def from(n: Int): LazyList[Int] = LazyList.cons(n, from(n + 1))
 
-  lazy val fibs: LazyList[Int] = ???
+  lazy val fibs: LazyList[Int] =
+    def go(curr: Int, next: Int): LazyList[Int] =
+      cons(curr, go(next, curr + next))
+    go(0,1)
 
-  def unfold[A, S](state: S)(f: S => Option[(A, S)]): LazyList[A] = ???
+  def unfold[A, S](state: S)(f: S => Option[(A, S)]): LazyList[A] =
+    f(state).map((a, s) => cons(a, unfold(s)(f))).getOrElse(empty[A])
 
-  lazy val fibsViaUnfold: LazyList[Int] = ???
+  lazy val fibsViaUnfold: LazyList[Int] =
+    unfold((0, 1))((curr, next) => Some(curr, (curr, curr + next)))
 
-  def fromViaUnfold(n: Int): LazyList[Int] = ???
+  def fromViaUnfold(n: Int): LazyList[Int] =
+    unfold(n)(n => Some(n, n + 1))
 
-  def continuallyViaUnfold[A](a: A): LazyList[A] = ???
+  def continuallyViaUnfold[A](a: A): LazyList[A] = {
+//    unfold(a)(n => Some(n, n))
+    unfold(())(_ => Some(a, ()))
+  }
 
-  lazy val onesViaUnfold: LazyList[Int] = ???
+  lazy val onesViaUnfold: LazyList[Int] =
+    unfold(())(_ => Some(1, ()))
--- src/main/scala/fpinscala/exercises/state/State.scala
+++ src/main/scala/fpinscala/exercises/state/State.scala
@@ -1,6 +1,5 @@
 package fpinscala.exercises.state
 
-
 trait RNG:
   def nextInt: (Int, RNG) // Should generate a random `Int`. We'll later define other functions in terms of `nextInt`.
 
@@ -26,27 +25,67 @@ object RNG:
       val (a, rng2) = s(rng)
       (f(a), rng2)
 
-  def nonNegativeInt(rng: RNG): (Int, RNG) = ???
+  def nonNegativeInt(rng: RNG): (Int, RNG) = {
+    val tar = int(rng)
+    if tar._1 < 0 then (-(tar._1 + 1), tar._2)
+    else tar
+  }
 
-  def double(rng: RNG): (Double, RNG) = ???
+  def double(rng: RNG): (Double, RNG) =
+    val (i, r) = nonNegativeInt(rng)
+    (i / (Int.MaxValue.toDouble + 1), r)
 
-  def intDouble(rng: RNG): ((Int,Double), RNG) = ???
+  def intDouble(rng: RNG): ((Int,Double), RNG) =
+    val (i, r1) = int(rng)
+    val (d, r2) = double(rng)
+    ((i, d), r2)
 
-  def doubleInt(rng: RNG): ((Double,Int), RNG) = ???
+  def doubleInt(rng: RNG): ((Double,Int), RNG) =
+    val ((i, d), r) = intDouble(rng)
+    ((d, i), r)
 
-  def double3(rng: RNG): ((Double,Double,Double), RNG) = ???
+  def double3(rng: RNG): ((Double,Double,Double), RNG) =
+    val (d1, r1) = double(rng)
+    val (d2, r2) = double(r1)
+    val (d3, r3) = double(r2)
+    ((d1, d2, d3), r3)
 
-  def ints(count: Int)(rng: RNG): (List[Int], RNG) = ???
+  def ints(count: Int)(rng: RNG): (List[Int], RNG) =
+    def go(currC: Int, rl: (List[Int], RNG)) : (List[Int], RNG) =
+      if(currC == 0)
+        rl
+      else
+        val (i, r) = int(rl._2)
+        go(currC-1, (i :: rl._1, r))
+    go(count, (List(), rng)) //递归，但顺序为倒序，
 
-  def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = ???
+  def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+    rng =>
+      val (a, rnga) = ra(rng)
+      val (b, rngb) = rb(rnga)
+      (f(a, b), rngb)
 
-  def sequence[A](rs: List[Rand[A]]): Rand[List[A]] = ???
+  def both[A,B](ra: Rand[A], rb: Rand[B]): Rand[(A,B)] =
+    map2(ra, rb)((_, _))
 
-  def flatMap[A, B](r: Rand[A])(f: A => Rand[B]): Rand[B] = ???
+  def sequence[A](rs: List[Rand[A]]): Rand[List[A]] =
+    rs.foldRight(unit(Nil: List[A]))((r, acc) => map2(r, acc)(_ :: _))
 
-  def mapViaFlatMap[A, B](r: Rand[A])(f: A => B): Rand[B] = ???
+  def flatMap[A, B](r: Rand[A])(f: A => Rand[B]): Rand[B] =
+    rng0 =>
+      val (a, rng1) = r(rng0)
+      f(a)(rng1)
 
-  def map2ViaFlatMap[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = ???
+  def nonNegativeLessThan(n: Int): Rand[Int] =
+    flatMap(nonNegativeInt): i =>
+      val mod = i % n
+      if i + (n-1) - mod >= 0 then unit(mod) else nonNegativeLessThan(n)
+
+  def mapViaFlatMap[A, B](r: Rand[A])(f: A => B): Rand[B] =
+    flatMap(r)(a => unit(f(a)))
+
+  def map2ViaFlatMap[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+    flatMap(both(ra, rb))((a, b) => unit(f(a, b)))
 
 opaque type State[S, +A] = S => (A, S)
 
@@ -55,15 +94,40 @@ object State:
     def run(s: S): (A, S) = underlying(s)
 
     def map[B](f: A => B): State[S, B] =
-      ???
+      flatMap(a => unit(f(a)))
 
     def map2[B, C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
-      ???
+      for
+        a <- run
+        b <- sb
+      yield f(a, b)
 
     def flatMap[B](f: A => State[S, B]): State[S, B] =
-      ???
+      s0 =>
+        val (a, s1) = run(s0)
+        f(a)(s1)
 
   def apply[S, A](f: S => (A, S)): State[S, A] = f
+
+  def unit[S, A](a: A): State[S, A] =
+    s => (a, s)
+
+  def sequence[S, A](actions: List[State[S, A]]): State[S, List[A]] =
+    actions.foldRight(unit[S, List[A]](Nil))((f, acc) => f.map2(acc)(_ :: _))
+
+  def traverse[S, A, B](as: List[A])(f: A => State[S, B]): State[S, List[B]] =
+    as.foldRight(unit[S, List[B]](Nil))((a, acc) => f(a).map2(acc)(_ :: _))
+
+  // S => (Unit, S)
+  def modify[S](f: S => S): State[S, Unit] =
+    for
+      s <- get // Gets the current state and assigns it to `s`.
+      _ <- set(f(s)) // Sets the new state to `f` applied to `s`.
+    yield ()
+
+  def get[S]: State[S, S] = s => (s, s)
+
+  def set[S](s: S): State[S, Unit] = _ => ((), s)
 
 enum Input:
   case Coin, Turn
